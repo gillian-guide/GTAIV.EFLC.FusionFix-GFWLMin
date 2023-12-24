c16841116ab182f4be314b3a5190363851f151cb
GillianMC <70141395+SandeMC@users.noreply.github.com>
2023-12-25 01:04:33 +0200

    Make a one-click script

--- release.bat
+++ release.bat
@@ -1,14 +1,17 @@
-copy bin\GTAIV.EFLC.FusionFix.asi data\plugins\GTAIV.EFLC.FusionFix.asi
+mkdir release
+copy bin\GTAIV.EFLC.FusionFix.asi release\GTAIV.EFLC.FusionFix.asi
+copy data\plugins\GTAIV.EFLC.FusionFix.ini release\GTAIV.EFLC.FusionFix.ini
 
 call buildimg.bat
 call buildshaders.bat
 
-7z a "GTAIV.EFLC.FusionFix.zip" ".\data\*" ^
--xr!update\GTAIV.EFLC.FusionFix\GTAIV.EFLC.FusionFix ^
--xr!update\GTAIV.EFLC.FusionFix\GTAIV.FusionFix ^
--xr!update\GTAIV.EFLC.FusionFix\TLAD.FusionFix ^
--xr!update\GTAIV.EFLC.FusionFix\TBOGT.FusionFix ^
--xr!update\GTAIV.EFLC.FusionFix\FusionTrees ^
--xr!update\GTAIV.EFLC.FusionFix\FusionLights ^
--x!update\GTAIV.EFLC.FusionFix\cdimagemake.ims ^
--x!update\common\shaders\.gitkeep
+xcopy /s /i data\update\common\data release\update\common\data
+xcopy /s /i data\update\common\shaders\win32_30 release\update\common\shaders\win32_30
+xcopy /s /i data\update\pc release\update\pc
+xcopy /s /i data\update\TBoGT release\update\TBoGT
+xcopy /s /i data\update\TLAD release\update\TLAD
+xcopy /s /i data\update\GTAIV.EFLC.FusionFix\IV release\update\GTAIV.EFLC.FusionFix\IV
+xcopy /s /i data\update\GTAIV.EFLC.FusionFix\TBOGT release\update\GTAIV.EFLC.FusionFix\TBOGT
+xcopy /s /i data\update\GTAIV.EFLC.FusionFix\TLAD release\update\GTAIV.EFLC.FusionFix\TLAD
+copy data\update\GTAIV.EFLC.FusionFix\GTAIV.EFLC.FusionFix.img release\update\GTAIV.EFLC.FusionFix\GTAIV.EFLC.FusionFix.img
+copy data\update\GTAIV.EFLC.FusionFix\FusionLights.img release\update\GTAIV.EFLC.FusionFix\FusionLights.img
