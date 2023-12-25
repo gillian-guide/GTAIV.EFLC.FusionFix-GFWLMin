8196671974644a5a26fc672cbe6cc99c8dcd54e4
GillianMC <70141395+SandeMC@users.noreply.github.com>
2023-12-25 13:28:42 +0200

    Further improve(?) the building script

--- release.bat
+++ release.bat
@@ -1,17 +1,12 @@
+del release
 mkdir release
+mkdir release\update\GTAIV.EFLC.FusionFix\IV
+mkdir release\update\GTAIV.EFLC.FusionFix\TLAD
+mkdir release\update\GTAIV.EFLC.FusionFix\TBoGT
 copy bin\GTAIV.EFLC.FusionFix.asi release\GTAIV.EFLC.FusionFix.asi
 copy data\plugins\GTAIV.EFLC.FusionFix.ini release\GTAIV.EFLC.FusionFix.ini
 
 call buildimg.bat
 call buildshaders.bat
 
-xcopy /s /i data\update\common\data release\update\common\data
-xcopy /s /i data\update\common\shaders\win32_30 release\update\common\shaders\win32_30
-xcopy /s /i data\update\pc release\update\pc
-xcopy /s /i data\update\TBoGT release\update\TBoGT
-xcopy /s /i data\update\TLAD release\update\TLAD
-xcopy /s /i data\update\GTAIV.EFLC.FusionFix\IV release\update\GTAIV.EFLC.FusionFix\IV
-xcopy /s /i data\update\GTAIV.EFLC.FusionFix\TBOGT release\update\GTAIV.EFLC.FusionFix\TBOGT
-xcopy /s /i data\update\GTAIV.EFLC.FusionFix\TLAD release\update\GTAIV.EFLC.FusionFix\TLAD
-copy data\update\GTAIV.EFLC.FusionFix\GTAIV.EFLC.FusionFix.img release\update\GTAIV.EFLC.FusionFix\GTAIV.EFLC.FusionFix.img
-copy data\update\GTAIV.EFLC.FusionFix\FusionLights.img release\update\GTAIV.EFLC.FusionFix\FusionLights.img
+xcopy /s /i data\update\ release\update /exclude:exclude.txt
